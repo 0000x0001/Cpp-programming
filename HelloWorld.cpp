32f5518b2aaefceffd7a33c56ee869badc4ab3e0
0000x0001 <42431270+0000x0001@users.noreply.github.com>
2018-08-22 18:13:36 -0700

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+//
+// HelloWorld.cpp
+//
+// WenLong Wu
+// 8/22/2018
+// Lab1
+// Print Hello World
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "This is my first cpp program!" << endl;
+   return 0;
+}
